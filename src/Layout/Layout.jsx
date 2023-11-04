899a71e94043ef65129264c360625d7c34b67eda
DmtrPpl <dmtrpopil@gmail.com>
2023-11-04 18:59:57 +0200

    Adding React Routers!

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer";
+import Routers from "../routes/Routers";
+import AuthorizationRouters from "../routes/AuthorizationRouters";
+
+const Layout = ( ) => {
+    return (
+        <div>
+            <AuthorizationRouters />
+            <main>
+                <Header />
+                <Routers />
+                <Footer />
+            </main>
+        </div>
+            
+        
+    );
+};
+
+export default Layout;
---
+++ src/pages/Venues/VenueDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VenueDetails() {
+  return (
+    <div>VenueDetails</div>
+  )
+}
+
+export default VenueDetails
