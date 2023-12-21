46174ba70409aa1cc850b4ad092d1a8991f4c0b6
DmtrPpl <dmtrpopil@gmail.com>
2023-12-21 22:34:22 +0200

    Added footer!

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import { Link } from 'react-router-dom';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-logo">Your Logo</div>
+        <div className="footer-links">
+          <Link to="/home">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="footer-social">
+        <Link to="#"><i className="fab fa-facebook"></i></Link>
+        <Link to="#"><i className="fab fa-twitter"></i></Link>
+        <Link to="#"><i className="fab fa-instagram"></i></Link>
+      </div>
+      <div className="footer-text">
+        <p>&copy; 2023 Your Company Name</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
