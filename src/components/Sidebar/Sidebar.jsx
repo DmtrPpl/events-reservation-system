2973ea995695c8226c282b2a703d6ebb84c20dc2
DmtrPpl <dmtrpopil@gmail.com>
2024-01-02 00:53:49 +0200

    Add some changes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -150,7 +150,7 @@ const Sidebar = () => {
               <div className="user-name">{name}</div>
               <div className="email">{email}</div>
             </div>
-            <Link href="#logout" className="logout hide">
+            <Link to="/login" className="logout hide">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24"
                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                 stroke-linejoin="round">
